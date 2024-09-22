aac2b63b43aecac8d02025acb0c4aa45b8a08bd1
Max <76400282+MaxSP10@users.noreply.github.com>
2024-09-22 13:40:39 -0500

    Imagen izquierda

--- src/Login.js
+++ src/Login.js
@@ -2,18 +2,25 @@ import React from 'react'
 import './Login.css'
 import logo from './images/logoalter.png'
 import { useNavigate } from 'react-router-dom';
+import examen from './images/imagenexamen.png'
 
 const Login = () => {
   const navigate = useNavigate();
 
   return (
     <div className='Login'>
-      <div className='Login-left'></div>
+      <div className='Login-left'>
+        <img src={examen} alt='examen' className='Login-examen' />
+      </div>
+
       <div className='Login-right'>
 
         <div className='Login-cuadro'>
+
           <img src={logo} alt='logo' className='Login-logo' />
           <h1 className='Login-title'>Iniciar sesión</h1>
+
+
           <div className='Login-form'>
             <label htmlFor='username'>Usuario:</label>
             <input type='text' id='username' name='username' placeholder='@utp.edu.pe' />
