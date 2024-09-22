495df69343558ccfbd49bc3ae0c61a84f8e9025c
Max <76400282+MaxSP10@users.noreply.github.com>
2024-09-22 11:05:26 -0500

    CSS responsivo texto

--- src/components/topbar.js
+++ src/components/topbar.js
@@ -3,16 +3,24 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 import logo from '../images/logo.png';
 import SideBar from './navbar';
 import { useState } from 'react';
-
+import photo from '../images/Profile.png';
+import '../styles/topbar.css';
 
 function TopBar() {
     const [showSideBar, setShowSideBar] = useState(false);
   return (
     <header className='topbar'>
         <GiHamburgerMenu onClick={() => setShowSideBar(!showSideBar)} className='menu-hamburger'/>
-        <div className='logo-container'>
-            <img src={logo} alt='logo' className='logo' />
+        <div className='main-content'>
+            <div className='logo-container'>
+                <img src={logo} alt='logo' className='logo' />
+            </div>
+            
+            <div className='photo-container'>
+                <img src={photo} alt='photo' className='photo' />
+            </div>
         </div>
+
         {showSideBar && <SideBar />}
     </header>
 
