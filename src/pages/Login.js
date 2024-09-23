01f4fd3a9135b9588a1a1183ee46de5d722c5ce3
Max <76400282+MaxSP10@users.noreply.github.com>
2024-09-22 20:38:21 -0500

    asdad

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,11 +2,7 @@ import React from 'react'
 import '../pagesstyles/Login.css'
 import logo from '../images/logoalter.png'
 import { useNavigate } from 'react-router-dom';
-<<<<<<< HEAD:src/Login.js
-import examen from './images/imagenexamen.png'
-=======
 import examen from '../images/imagenexamen.png'
->>>>>>> QuillLib:src/pages/Login.js
 
 const Login = () => {
   const navigate = useNavigate();
